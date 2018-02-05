a22d094c324712ba6aa0523877db4e70831d873b
esteban1306 <esteban1306@gmail.com>
2018-02-04 20:23:02 -0500

    u

--- config/app.php
+++ config/app.php
@@ -176,8 +176,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        Mariuzzo\LaravelJsLocalization\LaravelJsLocalizationServiceProvider::class,
-        Lord\Laroute\LarouteServiceProvider::class,
         Yajra\DataTables\DataTablesServiceProvider::class,
     ],
 
