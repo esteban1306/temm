c5227f18af41d357835780547aa8fffef55c945d
esteban1306 <esteban1306@gmail.com>
2018-02-05 23:44:06 -0500

    u

--- resources/views/app/nav_panel.blade.php
+++ resources/views/app/nav_panel.blade.php
@@ -3,7 +3,7 @@
         <section class="areaBread auto_margin">
             <div class="row">
                 <div class="col-6">
-                    <h1>Partners</h1>
+                    <h1>Que hoy sea un gran dia</h1>
                 </div>
                 <div class="col-6 text-right">
                     <span class="member">Hola, {{ Auth::user()->name }}{{ Auth::user()->partner_id }}{{ Auth::user()->parking_id }}</span>
@@ -13,14 +13,3 @@
         </section>
     </div>
 </div>
-<nav class="nav_patner_panel">
-    <div class="container-fluid">
-        <ul class="auto_margin">
-            <li class="active"><a href="partners-panel.html">Dashboard</a></li>
-            <li ><a href="partners-table-list.html">Mis referidos</a></li>
-            <li><a href="partners-table-list.html">Historial</a></li>
-            <li><a href="partners-table-list.html">Retirados </a><span class="badge badge-danger">6</span></li>
-            <li><a href="partners-cuenta.html">Mi Cuenta</a></li>
-        </ul>
-    </div>
-</nav>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,6 @@
     <body>
     @include('app.header')
     @yield('content')
-    @include('app/footer')
     @yield('scripts')
     </body>
 </html>
