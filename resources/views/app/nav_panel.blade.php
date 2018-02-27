95a61783280ef97fc57053fbdaeebe0ab9685acb
esteban1306 <esteban1306@gmail.com>
2018-02-26 22:02:55 -0500

    u

--- resources/views/app/nav_panel.blade.php
+++ resources/views/app/nav_panel.blade.php
@@ -2,10 +2,10 @@
     <div class="container-fluid">
         <section class="areaBread auto_margin">
             <div class="row">
-                <div class="col-6">
-                    <h1>Que hoy sea un gran día</h1>
+                <div class="col-9">
+                    <h1>"¿Que, pues, diremos a esto? Si Dios es por nosotros, ¿Quién contra nosotros?" : Romanos 8:31</h1>
                 </div>
-                <div class="col-6 text-right">
+                <div class="col-3 text-right">
                     <span class="member">Hola, {{ Auth::user()->name }}</span>
                     <figure><img src="images/user-partner.png" class="img-fluid" alt=""></figure>
                 </div>
