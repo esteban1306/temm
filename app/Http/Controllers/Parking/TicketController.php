a0163c27603d8129215a6c2cefeea613eb4e14f3
Juan Esteban <esteban@wasi.pro>
2018-04-11 10:41:44 -0500

    u

--- app/Http/Controllers/Parking/TicketController.php
+++ app/Http/Controllers/Parking/TicketController.php
@@ -95,7 +95,7 @@ class TicketController extends Controller
         $html = '<div style="text-align:center"><big style="margin-bottom: 1px"><b>PARQUEADERO '.$parking->name.'</b></big><br>
                 <em style="font-size: x-small;margin-top: 2px;margin-bottom: 1px">"Todo lo puedo en Cristo que<br> me fortalece": Fil 4:13 <br></em>
                 <small style="font-size: x-small;margin-top: 2px;margin-bottom: 1px"><b>'.$parking->address.'</b></small>'.($parking->parking_id==3?'<small style="text-align:center;font-size: 7px"><br>
-    SERVICIO: Lun-Sab 7am - 9pm<br>OLIVEROS HERNANDEZ VALENTINA <br> NIT: 1094965452 <br> TEL: 3104276986</small>':'');
+    SERVICIO: Lun-Sab 7am - 9pm<br>OLIVEROS HERNANDEZ VALENTINA <br> NIT: 1094965452_1 <br> TEL: 3104276986</small>':'');
         if(!isset($ticket->price)) {
             $html .= '<small style="text-align:left;font-size: small"><br>
                  Fecha ingreso: ' . $hour->format('d/m/Y') . '<br>
