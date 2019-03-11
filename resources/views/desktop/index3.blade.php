c7cd0a6b48c10638035b1507e74d6fb9ba5c801e
Juan Esteban <esteban@wasi.pro>
2019-03-11 14:24:46 -0500

    u

--- resources/views/desktop/index3.blade.php
+++ resources/views/desktop/index3.blade.php
@@ -1190,7 +1190,7 @@
                             }
                         });
                 },
-                deleteProduct : function(income) {
+                deleteIncome : function(income) {
                         $.ajax({
                             headers: {
                                 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
