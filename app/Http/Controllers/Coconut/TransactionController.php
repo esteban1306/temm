553a5dbc517b2ac1b7c6b90fdb5b5e02619d3d03
Juan Esteban <esteban@wasi.pro>
2019-03-13 17:37:26 -0500

    u

--- app/Http/Controllers/Coconut/TransactionController.php
+++ app/Http/Controllers/Coconut/TransactionController.php
@@ -192,7 +192,7 @@ class TransactionController extends Controller
 
                     ]).\Form::button('Editar', [
                             'class'   => 'btn btn-primary',
-                            'onclick' => "openModalVenta('$tickets->Id','".format_money($tickets->precio)."','$tickets->customer_id')",
+                            'onclick' => "openModalVenta('$tickets->Id','".format_money($tickets->precio)."','".($tickets->customer_id ?? '')."')",
                             'data-toggle' => "tooltip",
                             'data-placement' => "bottom",
                             'title' => "Editar !",
