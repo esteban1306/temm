951f3d55da5e39d7667fc242d0297d4853320d42
Juan Esteban <esteban@wasi.pro>
2019-03-14 16:59:40 -0500

    u

--- resources/views/desktop/index3.blade.php
+++ resources/views/desktop/index3.blade.php
@@ -6,7 +6,7 @@
             <!---->
             <div class="row">
                 <div class="col-md-6" style="text-align: center;">
-                    <button type="button" onclick="openModalVenta('','','')" class="btn btn-default col-md-10 btn-lg">Nueva Venta</button>
+                    <button type="button" onclick="openModalVenta('','','')" class="btn btn-info col-md-10 btn-lg">Nueva Venta</button>
                 </div>
             </div>
             <p class="height_10"></p>
@@ -132,7 +132,7 @@
                     <button type="button" onclick="openModalProduct()" class="btn btn-primary col-md-10 btn-lg">Nuevo Producto</button>
                 </div>
                 <div class="col-md-6" style="text-align: center;">
-                    <button type="button" onclick="openModalCliente()" class="btn btn-primary col-md-10 btn-lg">Nuevo Cliente</button>
+                    <button type="button" onclick="openModalCliente()" class="btn btn-secondary col-md-10 btn-lg">Nuevo Cliente</button>
                 </div>
             </div>
             <p class="height_10"></p>
