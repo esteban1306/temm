23c5d4d4b4f5f082ffed58009f5a3f9f45f84022
Juan Esteban <esteban@wasi.pro>
2019-03-28 08:57:18 -0500

    u

--- app/Http/Controllers/Coconut/IncomeController.php
+++ app/Http/Controllers/Coconut/IncomeController.php
@@ -106,8 +106,7 @@ class IncomeController extends Controller
 
         $income->save();
 
-        //$product->precio = ((($product->precio*1) * ($product->cantidad*1))+(($request->cantidad*1) * ($request->precio*1)))/(($product->cantidad*1) + ($income->cantidad*1));
-        $product->description = ($product->cantidad*($product->precio*1)) .'->'. ($income->cantidad*($income->precio*1));
+        $product->precio = intval((($product->cantidad*($product->precio*1)) + ($income->cantidad*($income->precio*1))) /($product->cantidad+$income->cantidad));
         $product->cantidad = ($product->cantidad*1) + ($income->cantidad*1);
         $product->save();
 
