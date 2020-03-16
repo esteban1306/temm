5eaa4cd4855387faba068c3c88547c4347d8faf3
esteban1306 <Matias0613>
2020-03-16 13:37:07 -0500

    u

--- app/Http/Controllers/Coconut/IncomeController.php
+++ app/Http/Controllers/Coconut/IncomeController.php
@@ -221,7 +221,7 @@ class IncomeController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show($id) 
     {
         //
     }
